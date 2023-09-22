34b25973ed75e4b83f19e27bf69a1d1a773b6b33
KartikLakra1 <kitrakarkal@gmail.com>
2023-09-22 09:39:14 +0530

    login controller and token generation

--- routers/authRoute.js
+++ routers/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {registerController} from '../controllers/authController.js'
+import {registerController , loginController} from '../controllers/authController.js'
 
 // router object
 const router = express.Router();
@@ -7,5 +7,8 @@ const router = express.Router();
 // routes congiguration
 // register || METHOD POST 
 router.post('/register' , registerController);
+
+// LOGIN || post
+router.post('/login' , loginController);
 
 export default router;
